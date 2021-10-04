a9d26e130fba4fbb7d8675f777cb2ed6e842fe92
prazeina <prazeina@gmail.com>
2021-10-04 23:15:58 +0545

    Day-11: Simple logging

---
+++ Day_11.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'selenium-webdriver'
+require 'chromedriver-helper'
+
+driver = Selenium::WebDriver.for :chrome
+Selenium::WebDriver.logger.output = "selenium.log"
+
+# navigate to page
+
+describe "File Picker" do
+    it "Uploads a picture" do
+        driver.get 'https://kitchen.applitools.com/ingredients/file-picker'
+        driver.find_element(:xpath, '//*[@id="photo-upload"]').send_keys "/home/prazeina/repos/prazeina/Selenium/Selenium/Baji.png"
+        sleep 5
+    end
+end
